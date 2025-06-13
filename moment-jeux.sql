b459a1afe1fc77a34af0724b606364d448354037
01Chloe <allier.chloe@gmaim.com>
2025-06-13 16:11:28 +0200

    add question 2 to 6

--- moment-jeux.sql
+++ moment-jeux.sql
@@ -3,3 +3,43 @@ SELECT *
 FROM `game` 
 ORDER BY game.published_at DESC 
 LIMIT 9;
+
+-- 2
+SELECT COUNT(*), game.name 
+FROM `game` 
+JOIN user_own_game ON user_own_game.game_id = game.id 
+GROUP BY user_own_game.game_id 
+ORDER BY COUNT(*) DESC 
+LIMIT 9;
+
+-- 3
+SELECT game.name
+FROM `user_own_game`
+JOIN game ON game.id = user_own_game.game_id
+GROUP BY game.name
+ORDER BY user_own_game.game_time DESC
+LIMIT 9;
+
+-- 4
+SELECT user.nickname, game.name,review.rating, review.up_vote, review.down_vote
+FROM `review`
+JOIN user ON user.id = review.user_id
+JOIN game ON game.id = review.game_id
+ORDER BY review.created_at DESC
+LIMIT 4;
+
+-- 5
+SELECT DISTINCT category.name
+FROM `game_category`
+JOIN user_own_game ON user_own_game.game_id = game_category.game_id
+JOIN category ON game_category.category_id = category.id
+ORDER BY user_own_game.game_time DESC 
+LIMIT 9;
+
+-- 6
+SELECT user.nickname, SUM(user_own_game.game_time) AS 'temps de jeux total'
+FROM user
+JOIN user_own_game ON user_own_game.user_id = user.id
+GROUP BY user.nickname;
+
+-- 7
